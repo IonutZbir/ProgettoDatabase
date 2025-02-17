772548c0642fe881946646f8d022596e48544b42
Ghija <franci2002.fc@gmail.com>
2025-02-17 18:23:09 +0100

    aggiunta query

--- provaQuery.sql
+++ provaQuery.sql
@@ -3,7 +3,7 @@ USE Torverbarber;
 SELECT o.stato AS StatoOrdine, d.Prodotto, p.Nome AS NomeProdotto
 FROM Ordine o
 JOIN DettaglioOrdine d ON d.OrdineId = o.OrdineId
-JOIN Prodotto p ON p.ProdottoID = d.ProdottoID
+JOIN Prodotto p ON p.ProdottoId = d.ProdottoId
 JOIN Cliente c ON o.ClienteId = c.ClienteId
 WHERE c.nome = '' AND c.cognome = '';
 
@@ -21,7 +21,7 @@ USE Torverbarber;
 
 SELECT p.data, p.ora, c.nome, c.cognome, n.nome AS NomeNegozio
 FROM Prenotazione p
-JOIN Cliente c ON c.ClienteId = p.ClienteID
+JOIN Cliente c ON c.ClienteId = p.ClienteId
 JOIN Negozio n ON n.NegozioId = p.NegozioId
 ORDER BY p.data, p.ora;
 
@@ -29,7 +29,7 @@ USE Torverbarber;
 
 SELECT p.nome, p.categoria, i.quantita, n.nome AS NomeNegozio
 FROM Inventario i
-JOIN Prodotto p ON i.ProdottoID = p.ProdottoID
+JOIN Prodotto p ON i.ProdottoId = p.ProdottoId
 JOIN Negozio n ON i.NegozioId = n.NegozioId
 WHERE n.nome = '';
 
@@ -37,7 +37,7 @@ USE Torverbarber;
 
 SELECT o.dataOrdine, o.stato
 FROM Prodotto paese
-JOIN DettaglioOrdine d ON d.ProdottoId = p.ProdottoID
+JOIN DettaglioOrdine d ON d.ProdottoId = p.ProdottoId
 JOIN Ordine o ON o.OrdineId = d.OrdineId
 WHERE p.nome = '';
 
@@ -48,3 +48,19 @@ FROM ApplicaOfferta a
 JOIN Offerta o ON a.OffertaId = o.OffertaId
 JOIN Prodotto p ON a.ProdottoId = p.ProdottoId
 WHERE p.nome = '';
+
+USE Torverbarber;
+
+SELECT f.valutazione, f.commento, f.dataFeedback
+FROM Feedback f
+JOIN Cliente c ON c.ClienteId = f.ClienteId
+WHERE c.nome = '' and c.cognome = '';
+
+USE Torverbarber;
+
+SELECT c.nome, c.cognome, f.valutazione, f.commento, f.dataFeedback
+FROM Feedback f
+JOIN Cliente c ON f.ClienteId = c.ClienteId
+JOIN Prenotazione p ON f.PrenotazioneId = p.PrenotazioneId
+JOIN Dipendente d ON d.DipendenteId = p.DipendenteId
+WHERE c.nome = '' and c.cognome = '';
