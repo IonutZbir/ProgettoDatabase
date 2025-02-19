86523b7045a059cd2c01c553cb3588b6e37b4cc3
ionut <ionut.roma9@gmail.com>
2025-02-19 12:37:15 +0100

    query

--- provaQuery.sql
+++ provaQuery.sql
@@ -1,116 +1,220 @@
 USE Torverbarber;
 
-SELECT o.stato AS StatoOrdine, d.Prodotto, p.Nome AS NomeProdotto
-FROM Ordine o
-JOIN DettaglioOrdine d ON d.OrdineId = o.OrdineId
-JOIN Prodotto p ON p.ProdottoId = d.ProdottoId
-JOIN Cliente c ON o.ClienteId = c.ClienteId
-WHERE c.nome = '' AND c.cognome = '';
-
-
-USE Torverbarber;
-
-SELECT d.dataAssunzione, d.nome, d.cognome, r.tipoRuolo
-FROM Dipendente data
-JOIN Ruolo r ON d.RuoloId = r.RuoloId
-JOIN Negozio n ON n.NegozioId =  d.NegozioId
-ORDER BY r.tipoRuolo, d.dataAssunzione 
-WHERE n.nome = '';
+SELECT
+    o.stato AS StatoOrdine,
+    d.Prodotto,
+    p.Nome AS NomeProdotto
+FROM
+    Ordine o
+    JOIN DettaglioOrdine d ON d.OrdineId = o.OrdineId
+    JOIN Prodotto p ON p.ProdottoId = d.ProdottoId
+    JOIN Cliente c ON o.ClienteId = c.ClienteId
+WHERE
+    c.nome = ''
+    AND c.cognome = '';
 
 USE Torverbarber;
 
-SELECT p.data, p.ora, c.nome, c.cognome, n.nome AS NomeNegozio
-FROM Prenotazione p
-JOIN Cliente c ON c.ClienteId = p.ClienteId
-JOIN Negozio n ON n.NegozioId = p.NegozioId
-ORDER BY p.data, p.ora;
+SELECT
+    d.dataAssunzione,
+    d.nome,
+    d.cognome,
+    r.tipoRuolo
+FROM
+    Dipendente data
+    JOIN Ruolo r ON d.RuoloId = r.RuoloId
+    JOIN Negozio n ON n.NegozioId = d.NegozioId
+ORDER BY
+    r.tipoRuolo,
+    d.dataAssunzione
+WHERE
+    n.nome = '';
 
 USE Torverbarber;
 
-SELECT p.nome, p.categoria, i.quantita, n.nome AS NomeNegozio
-FROM Inventario i
-JOIN Prodotto p ON i.ProdottoId = p.ProdottoId
-JOIN Negozio n ON i.NegozioId = n.NegozioId
-WHERE n.nome = '';
+SELECT
+    p.data,
+    p.ora,
+    c.nome,
+    c.cognome,
+    n.nome AS NomeNegozio
+FROM
+    Prenotazione p
+    JOIN Cliente c ON c.ClienteId = p.ClienteId
+    JOIN Negozio n ON n.NegozioId = p.NegozioId
+ORDER BY
+    p.data,
+    p.ora;
 
 USE Torverbarber;
 
-SELECT o.dataOrdine, o.stato
-FROM Prodotto paese
-JOIN DettaglioOrdine d ON d.ProdottoId = p.ProdottoId
-JOIN Ordine o ON o.OrdineId = d.OrdineId
-WHERE p.nome = '';
+SELECT
+    p.nome,
+    p.categoria,
+    i.quantita,
+    n.nome AS NomeNegozio
+FROM
+    Inventario i
+    JOIN Prodotto p ON i.ProdottoId = p.ProdottoId
+    JOIN Negozio n ON i.NegozioId = n.NegozioId
+WHERE
+    n.nome = '';
 
 USE Torverbarber;
 
-SELECT o.tipoOfferta, a.dataInizio, a.dataFine, a.PercentualeSconto, a.PrezzoPromozionale, o.tipoOfferta, o.descrizione
-FROM ApplicaOfferta a
-JOIN Offerta o ON a.OffertaId = o.OffertaId
-JOIN Prodotto p ON a.ProdottoId = p.ProdottoId
-WHERE p.nome = '';
+SELECT
+    o.dataOrdine,
+    o.stato
+FROM
+    Prodotto paese
+    JOIN DettaglioOrdine d ON d.ProdottoId = p.ProdottoId
+    JOIN Ordine o ON o.OrdineId = d.OrdineId
+WHERE
+    p.nome = '';
 
 USE Torverbarber;
 
-SELECT f.valutazione, f.commento, f.dataFeedback
-FROM Feedback f
-JOIN Cliente c ON c.ClienteId = f.ClienteId
-WHERE c.nome = '' and c.cognome = '';
+SELECT
+    o.tipoOfferta,
+    a.dataInizio,
+    a.dataFine,
+    a.PercentualeSconto,
+    a.PrezzoPromozionale,
+    o.tipoOfferta,
+    o.descrizione
+FROM
+    ApplicaOfferta a
+    JOIN Offerta o ON a.OffertaId = o.OffertaId
+    JOIN Prodotto p ON a.ProdottoId = p.ProdottoId
+WHERE
+    p.nome = '';
 
 USE Torverbarber;
 
-SELECT c.nome, c.cognome, f.valutazione, f.commento, f.dataFeedback
-FROM Feedback f
-JOIN Cliente c ON f.ClienteId = c.ClienteId
-JOIN Prenotazione p ON f.PrenotazioneId = p.PrenotazioneId
-JOIN Dipendente d ON d.DipendenteId = p.DipendenteId
-WHERE c.nome = '' and c.cognome = '';
+SELECT
+    f.valutazione,
+    f.commento,
+    f.dataFeedback
+FROM
+    Feedback f
+    JOIN Cliente c ON c.ClienteId = f.ClienteId
+WHERE
+    c.nome = ''
+    and c.cognome = '';
 
 USE Torverbarber;
 
-SELECT SUM(e.importo) AS TotaleEntrate, n.nome
-FROM Entrata e
-JOIN Negozio n on e.NegozioId = n.NegozioId
-WHERE e.dataEntrata BETWEEN '2020-11-01' AND '2020-12-01'
-GROUP BY n.NegozioId;
+SELECT
+    c.nome,
+    c.cognome,
+    f.valutazione,
+    f.commento,
+    f.dataFeedback
+FROM
+    Feedback f
+    JOIN Cliente c ON f.ClienteId = c.ClienteId
+    JOIN Prenotazione p ON f.PrenotazioneId = p.PrenotazioneId
+    JOIN Dipendente d ON d.DipendenteId = p.DipendenteId
+WHERE
+    c.nome = ''
+    and c.cognome = '';
 
 USE Torverbarber;
 
-CREATE VIEW PezziVenduti AS
-SELECT p.nome, SUM(d.quantita) AS PezziVenduti
-FROM Prodotto p
-JOIN DettaglioOrdine d ON d.ProdottoId = p.ProdottoId
-JOIN Ordine o ON o.OrdineId = d.OrdineId
-WHERE o.stato = 'Consegnato'
-GROUP BY p.nome;
+SELECT
+    SUM(e.importo) AS TotaleEntrate,
+    n.nome
+FROM
+    Entrata e
+    JOIN Negozio n on e.NegozioId = n.NegozioId
+WHERE
+    e.dataEntrata BETWEEN '2020-11-01' AND '2020-12-01'
+GROUP BY
+    n.NegozioId;
 
 USE Torverbarber;
 
-SELECT DATE_FORMAT(p.dataPrenotazione, '%Y-%m') AS mesePrenotazione, d.nome, d.cognome, AVG(p.PrenotazioneId) AS mediaPrenotazioni
-FROM Dipendente d 
-JOIN Prenotazione p ON p.DipendenteId = d.DipendenteId
-GROUP BY d.DipendenteId, mesePrenotazione;
+CREATE VIEW
+    PezziVenduti AS
+SELECT
+    p.nome,
+    SUM(d.quantita) AS PezziVenduti
+FROM
+    Prodotto p
+    JOIN DettaglioOrdine d ON d.ProdottoId = p.ProdottoId
+    JOIN Ordine o ON o.OrdineId = d.OrdineId
+WHERE
+    o.stato = 'Consegnato'
+GROUP BY
+    p.nome;
 
 USE Torverbarber;
 
-SELECT ((SELECT COUNT(p.PrenotazioneId) FROM Prenotazione p WHERE p.stato = 'Annullato') / COUNT(p.PrenotazioneId)) AS PrenotazioniTotalirenotazioniTotali
-FROM Prenotazione p
+SELECT
+    DATE_FORMAT (p.dataPrenotazione, '%Y-%m') AS mesePrenotazione,
+    d.nome,
+    d.cognome,
+    AVG(p.PrenotazioneId) AS mediaPrenotazioni
+FROM
+    Dipendente d
+    JOIN Prenotazione p ON p.DipendenteId = d.DipendenteId
+GROUP BY
+    d.DipendenteId,
+    mesePrenotazione;
 
 USE Torverbarber;
 
-SELECT c.nome, c.cognome, ((SUM(d.quantita) * p.prezzo) + (s.prezzo * COUNT(p1.PrenotazioneId)))
-FROM Cliente c
-JOIN Ordine o ON c.ClienteId = o.ClienteId
-JOIN DettaglioOrdine d ON o.OrdineId = d.OrdineId
-JOIN Prodotto p ON d.ProdottoId = p.ProdottoId
-JOIN Prenotazione p1 ON p1.ClienteId = c.ClienteId
-JOIN Servizio s ON p1.ServizioId = s.ServizioId
-GROUP BY c.ClienteId
-WHERE o.stato = 'Consegnato', p1.stato = 'Completato', o.dataOrdine BETWEEN '2024-12-01' AND '2024-06-01', p1,dataPrenotazione BETWEEN '2024-12-01' AND '2024-06-01';
+SELECT
+    (
+        (
+            SELECT
+                COUNT(p.PrenotazioneId)
+            FROM
+                Prenotazione p
+            WHERE
+                p.stato = 'Annullato'
+        ) / COUNT(p.PrenotazioneId)
+    ) AS PrenotazioniTotalirenotazioniTotali
+FROM
+    Prenotazione p USE Torverbarber;
+
+SELECT
+    c.nome,
+    c.cognome,
+    (
+        (SUM(d.quantita) * p.prezzo) + (s.prezzo * COUNT(p1.PrenotazioneId))
+    )
+FROM
+    Cliente c
+    JOIN Ordine o ON c.ClienteId = o.ClienteId
+    JOIN DettaglioOrdine d ON o.OrdineId = d.OrdineId
+    JOIN Prodotto p ON d.ProdottoId = p.ProdottoId
+    JOIN Prenotazione p1 ON p1.ClienteId = c.ClienteId
+    JOIN Servizio s ON p1.ServizioId = s.ServizioId
+GROUP BY
+    c.ClienteId
+WHERE
+    o.stato = 'Consegnato',
+    p1.stato = 'Completato',
+    o.dataOrdine BETWEEN '2024-12-01' AND '2024-06-01',
+    p1,
+    dataPrenotazione BETWEEN '2024-12-01' AND '2024-06-01';
 
 USE Torverbarber;
 
-SELECT c.nome, c.cognome, COUNT(o.OrdineId) AS OrdiniTotali
-FROM Cliente c 
-JOIN Ordine o ON c.ClienteId = o.ClienteId
-WHERE (SELECT COUNT(o.OrdineId) FROM Ordine o) > 3
-GROUP BY c.ClienteId;
+SELECT
+    c.nome,
+    c.cognome,
+    COUNT(o.OrdineId) AS OrdiniTotali
+FROM
+    Cliente c
+    JOIN Ordine o ON c.ClienteId = o.ClienteId
+WHERE
+    (
+        SELECT
+            COUNT(o.OrdineId)
+        FROM
+            Ordine o
+    ) > 3
+GROUP BY
+    c.ClienteId;
--- sql/query.sql
+++ sql/query.sql
@@ -175,12 +175,11 @@ FROM
 WHERE
     c.nome = 'Mario'
     and c.cognome = 'Rossi'
-
---- Visualizzare tutti i feedback relativi ad un dipendente
-USE Torverbarber;
+    --- Visualizzare tutti i feedback relativi ad un dipendente
+    USE Torverbarber;
 
 CREATE VIEW
-    Feedback_Per_Dipendente
+    Feedback_Per_Dipendente AS
 SELECT
     d.DipendenteId,
     p.dataPrenotazione,
@@ -200,4 +199,117 @@ WHERE
     AND d.cognome = 'Gianinazzi';
 
 --- Determina le entrate totali di un negozio in una specifica giornata.
+USE Torverbarber;
 
+SELECT
+    n.nome AS Nome_Negozio,
+    SUM(e.importo) AS Totale_Entrate
+FROM
+    Entrata e
+    JOIN Negozio n on e.NegozioId = n.NegozioId
+WHERE
+    e.dataEntrata BETWEEN '2024-11-01' AND '2024-12-01'
+GROUP BY
+    n.NegozioId;
+
+--- Calcolo delle vendite totali per prodotto
+USE Torverbarber;
+
+SELECT
+    p.nome AS Prodotto,
+    p.codiceBarre,
+    SUM(d.quantita) AS PezziVenduti
+FROM
+    Prodotto p
+    JOIN DettaglioOrdine d ON d.ProdottoId = p.ProdottoId
+    JOIN Ordine o ON o.OrdineId = d.OrdineId
+WHERE
+    o.stato = 'Consegnato'
+GROUP BY
+    p.nome;
+
+--- Numero medio di appuntamenti per dipendente al mese
+USE Torverbarber;
+
+SELECT
+    DATE_FORMAT (p.dataPrenotazione, '%M %Y') AS mesePrenotazione,
+    d.nome,
+    d.cognome,
+    ROUND(
+        COUNT(p.PrenotazioneId) / COUNT(
+            DISTINCT DATE_FORMAT (p.dataPrenotazione, '%Y-%m')
+        ),
+        1
+    ) AS mediaPrenotazioni
+FROM
+    Dipendente d
+    JOIN Prenotazione p ON p.DipendenteId = d.DipendenteId
+GROUP BY
+    d.DipendenteId,
+    mesePrenotazione;
+
+--- Percentuale di prenotazioni cancellate rispetto al totale
+USE Torverbarber;
+
+SELECT
+    COUNT(*) AS TotalePrenotazioni,
+    COUNT(
+        CASE
+            WHEN p.stato = 'Annullato' THEN 1
+        END
+    ) AS PrenotazioniCancellate,
+    (
+        COUNT(
+            CASE
+                WHEN p.stato = 'Annullato' THEN 1
+            END
+        ) * 100.0
+    ) / NULLIF(COUNT(*), 0) AS PercentualeCancellazioni
+FROM
+    Prenotazione p;
+
+--- Elenco dei clienti con il totale speso negli ultimi sei mesi
+SELECT
+    c.nome,
+    c.cognome,
+    (
+        COALESCE(SUM(d.quantita * p.prezzo), 0) + COALESCE(SUM(s.prezzo), 0)
+    ) AS TotaleSpeso
+FROM
+    Cliente c
+    LEFT JOIN Ordine o ON c.ClienteId = o.ClienteId
+    AND o.stato = 'Consegnato'
+    LEFT JOIN DettaglioOrdine d ON o.OrdineId = d.OrdineId
+    LEFT JOIN Prodotto p ON d.ProdottoId = p.CodiceBarre
+    LEFT JOIN Prenotazione p1 ON p1.ClienteId = c.ClienteId
+    AND p1.stato = 'Completato'
+    LEFT JOIN Servizio s ON p1.ServizioId = s.ServizioId
+WHERE
+    (
+        o.dataOrdine BETWEEN '2024-06-01' AND '2024-12-01'
+        OR p1.dataPrenotazione BETWEEN '2024-06-01' AND '2024-12-01'
+    )
+GROUP BY
+    c.ClienteId,
+    c.nome,
+    c.cognome
+ORDER BY
+    TotaleSpeso DESC;
+
+--- Lista dei clienti che hanno acquistato almeno 3 volte nell’ultimo anno
+
+--- Elenco di tutti i feedback con valutazione maggiore di 4
+SELECT
+    d.nome,
+    d.cognome,
+    f.valutazione,
+    f.dataFeedback,
+    f.commento
+FROM
+    Dipendente d
+    JOIN Prenotazione p ON p.DipendenteId = d.DipendenteId
+    JOIN Ruolo r ON r.RuoloId = d.RuoloId
+    JOIN Feedback f ON p.PrenotazioneId = f.FeedbackId
+WHERE
+    f.valutazione >= 4
+    AND r.tipoRuolo = 'Barbiere';
